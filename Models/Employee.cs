417712e1390987b6e12f90db818c992f6c9dce3b
Rajneesh Chaubey <rajneesh.chaubey@vergehealth.com>
2021-08-31 22:08:56 +0530

    #main-Initial Commit

---
+++ Models/Employee.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNetCrud.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public Department DepartmentId { get; set; }
+    }
+}
